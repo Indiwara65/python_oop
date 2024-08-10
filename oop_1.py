7773d11b3ce65af409fea045044a0e117caf8d87
Indiwara <indiwarananayakkara1@gmail.com>
2024-08-10 17:56:28 +0530

    class methods

--- oop_1.py
+++ oop_1.py
@@ -18,24 +18,28 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * Employee.raise_amount)  # or self.raise_amount
 
+    @classmethod
+    def set_raise_amount(cls, amount:float):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split("-")
+        return cls(first, last, int(pay)) 
+
 
 
 if __name__ == '__main__':
-    print(Employee.num_of_emps)
-    emp_1 = Employee("Silvia", "Nissan", 200000)
-    print(Employee.num_of_emps)
-    emp_2 = Employee("Camry", "Toyota", 180000)
-    print(Employee.num_of_emps,'\n')
+    emp_1_str = "Silvia-Nissan-200000"
+    emp_2_str = "Camry-Toyota-180000"
+    emp_1 = Employee.from_string(emp_1_str)
+    emp_2 = Employee.from_string(emp_2_str)
 
-    print(Employee.raise_amount)
-    emp_1.raise_amount = 1.05
-    print(Employee.raise_amount)
-    print(emp_1.raise_amount)
-    print(emp_2.raise_amount)
+    print(emp_1.email)
+    print(emp_2.email)
+    print("\n")
     #
-    print('\n')
-    #
-    Employee.raise_amount = 1.045
+    Employee.set_raise_amount(1.06)
     print(Employee.raise_amount)
     print(emp_1.raise_amount)
     print(emp_2.raise_amount)
