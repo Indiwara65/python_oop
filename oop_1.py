c47a85e4244c710d3a2af64d33d2a675cd18bbd3
Indiwara <indiwarananayakkara1@gmail.com>
2024-08-10 20:44:15 +0530

    Inheritance-Sub Classes

--- oop_1.py
+++ oop_1.py
@@ -37,14 +37,65 @@ class Employee:
             return False
         return True
 
+class Developer(Employee):
+    raise_amount = 1.2
 
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+    
+    def add_emp(self, employee):
+        if employee not in self.employees:
+            self.employees.append(employee)
+    
+    def remove_emp(self, employee):
+        if employee in self.employees:
+            self.employees.remove(employee)
+    
+    def print_emps(self):
+        for employee in self.employees:
+            print(employee.fullname())
 
 
 if __name__ == '__main__':
-    emp_1_str = "Silvia-Nissan-200000"
-    emp_2_str = "Camry-Toyota-180000"
-    emp_1 = Employee.from_string(emp_1_str)
-    emp_2 = Employee.from_string(emp_2_str)
+    emp_1 = Employee("Creseda", "Toyota", 160000)
+    emp_2 = Employee("Covette", "Chevy", 170000)
+    #
+    dev_1 = Developer("Silvia", "Nissan", 200000, "Python")
+    dev_2 = Developer("Camry", "Toyota", 180000, "Java")
+    #
+    emp_man = Manager("Patrol", "Nissan", 4250000, [emp_1, emp_2])
+    dev_man = Manager("Cruiser", "Toyota", 450000, [dev_1, dev_2])
 
-    my_date = datetime.date(2024,8,12)
-    print(Employee.is_workday(my_date))
+    
+    print(emp_1.email)
+    print(emp_2.email)
+    print('\n')
+    #
+    print(dev_1.email)
+    print(dev_2.email)
+    print('\n')
+    #
+    print(dev_1.prog_lang)
+    print(dev_2.prog_lang)
+    print("\n")
+    #
+    print(f"{emp_man.fullname()}:")
+    Manager.print_emps(emp_man)
+    print("\n")
+    print(f"{dev_man.fullname()}: ")
+    Manager.print_emps(dev_man)
+
+    #
+    print("\n")
+    print(isinstance(emp_man, Manager))
+    print(issubclass(Employee, Manager))
