5408da757e89e4cc5d187f2cdb769a66c20ac3b2
Indiwara <indiwarananayakkara1@gmail.com>
2024-08-10 17:32:36 +0530

    class variables

--- oop_1.py
+++ oop_1.py
@@ -1,21 +1,41 @@
 class Employee:
+    #class variables
+    raise_amount = 1.04
+    num_of_emps = 0
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = f"{first}.{last}@company.com"
 
+        Employee.num_of_emps += 1
+
      #methods
     def fullname(self):
         return f"{self.first} {self.last}"
     
     def apply_raise(self):
-        self.pay = int(self.pay * 1.04)
+        self.pay = int(self.pay * Employee.raise_amount)  # or self.raise_amount
+
 
 
 if __name__ == '__main__':
+    print(Employee.num_of_emps)
     emp_1 = Employee("Silvia", "Nissan", 200000)
+    print(Employee.num_of_emps)
     emp_2 = Employee("Camry", "Toyota", 180000)
+    print(Employee.num_of_emps,'\n')
 
-    print(emp_1.fullname())
-    print(Employee.fullname(emp_2))
+    print(Employee.raise_amount)
+    emp_1.raise_amount = 1.05
+    print(Employee.raise_amount)
+    print(emp_1.raise_amount)
+    print(emp_2.raise_amount)
+    #
+    print('\n')
+    #
+    Employee.raise_amount = 1.045
+    print(Employee.raise_amount)
+    print(emp_1.raise_amount)
+    print(emp_2.raise_amount)
