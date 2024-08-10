9f65d6cf5ba5963da6c56b76b5610fc7183f4a9b
Indiwara <indiwarananayakkara1@gmail.com>
2024-08-10 17:17:08 +0530

    methods

--- oop_1.py
+++ oop_1.py
@@ -5,10 +5,17 @@ class Employee:
         self.pay = pay
         self.email = f"{first}.{last}@company.com"
 
+     #methods
+    def fullname(self):
+        return f"{self.first} {self.last}"
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * 1.04)
+
 
 if __name__ == '__main__':
     emp_1 = Employee("Silvia", "Nissan", 200000)
     emp_2 = Employee("Camry", "Toyota", 180000)
 
-    print(emp_1.email)
-    print(emp_2.email)
+    print(emp_1.fullname())
+    print(Employee.fullname(emp_2))
